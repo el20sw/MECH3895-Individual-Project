c67224e6ed58252672de5c9728ebda4adf29cdc0
el20sw <el20sw@leeds.ac.uk>
2023-01-11 12:17:49 +0000

    add todo notes

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from testing.base_test import base_test
+from testing.random_test import random_test
 
 if __name__ == '__main__':
     base_test()
+    random_test()
     print('All tests passed')
+
+# TODO: Make proper pipeline network
+# TODO: Get multiple agents simulatenously in the same network
