bb7c61cab8849956ebf3ee1ed1aca2241cc3b797
el20sw <el20sw@leeds.ac.uk>
2023-01-28 13:40:35 +0000

    logging

--- src/simulation.py
+++ src/simulation.py
@@ -1,4 +1,8 @@
-import wntr
+# Import logger
+import debug.logger as logger
+
+### Import modules
+from src.overwatch import OverWatch
 
 ### Pipe Network Simulation ###
 class Simulation:
@@ -7,21 +11,40 @@ class Simulation:
         Class to simulate the pipe network environment
         :param environment: Environment to simulate
         """
+        # Initialise the logger
+        self.log = logger.get_logger(__name__)
+        # Initialise the environment
         self.environment = environment
         # Initialise the agents
         self.agents = []
+        self.num_agents = 0
         # Variables
         self.turns = 0
         self.pct_explored = 0
         self.visited_nodes = []
+        # Initialise the overwatch
+        self.ow = OverWatch(self.environment, self.agents)
+        self.ow_agent_positions = {}
+        self.ow_visited_nodes = []
 
     def add_agent(self, agent):
         """
-        Method to add an agent to the simulation
-        :param agent: Agent to add
+        Method to add an agent to the simulation - can be used to add multiple agents
+        :param agent: Agent/s to add
         :return: None
         """
-        self.agents.append(agent)
+        # if multiple agents are passed
+        if isinstance(agent, list):
+            for a in agent:
+                self.agents.append(a)
+                self.log.info(f"Agent {a.agent_id} added to simulation")
+        # if a single agent is passed
+        else:
+            self.agents.append(agent)
+            self.log.info(f"Agent {agent.agent_id} added to simulation")
+
+        # Update the number of agents
+        self.num_agents = len(self.agents)
 
     def run(self, max_turns=100):
         """
@@ -31,8 +54,19 @@ class Simulation:
         """
         # Run the simulation for a maximum number of turns
         while self.turns < max_turns and self.pct_explored < 100:
+            # Run one step of the simulation
             self.step()
+            # Update the results
+            self.update_results()
+            # Update the overwatch
+            self.ow.update()
+            # Increment the turn (local turn count)
             self.turns += 1
+            # Log the turn
+            self.log.info(f"Turn {self.turns} completed")
+            # Log the agents position
+            for agent in self.agents:
+                self.log.info(f"Agent {agent.agent_id} is at node {agent.position}")
 
     def step(self):
         """
@@ -50,23 +84,39 @@ class Simulation:
             # Communicate with other agents
             agent.communicate(self.environment)
 
+    def update_results(self):
+        """
+        Method to update the results of the simulation - interface with overwatch
+        :return: None
+        """
+        # Get the agent positions
+        self.ow_agent_positions = self.ow.get_agent_positions()
+        # Get the nodes that have been visited by all agents
+        self.ow_visited_nodes = self.ow.get_visited_nodes()
+        # Log the number of nodes explored
+        self.log.info(f"{len(self.ow_visited_nodes)} nodes explored")
+
+        # Get the percentage of nodes explored (from the overwatch)
+        self.pct_explored = self.ow.get_pct_explored()
+        # Log the percentage of nodes explored
+        self.log.info(f"{self.pct_explored}% of nodes explored")
+
+
     def get_results(self):
         """
         Method to get the results of the simulation
         :return: Results of the simulation
         """
-
-        self.visited_nodes = (agent.visited_nodes for agent in self.agents)
-
+       # Packaged results
         results = {
             'num_agents': len(self.agents),
             'turns': self.turns,
-            'pct_explored': len(set(self.visited_nodes)) / len(self.environment.get_node_names() * 100)
+            'pct_explored': self.pct_explored
         }
 
         return results
 
-    def write_results_to_file(self, file_path='Results/results.txt'):
+    def write_results_to_file(self, file_path='results/results.txt'):
         """
         Method to write the results of the simulation to a file
         :param file_path: Path to the file
