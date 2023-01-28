b6dc98cb9231070203e2cad16d0150e56ad66831
el20sw <el20sw@leeds.ac.uk>
2023-01-28 16:26:17 +0000

    logging fixes

--- src/overwatch.py
+++ src/overwatch.py
@@ -13,13 +13,13 @@ class OverWatch:
         self.agents = agents
         self.num_agents = len(agents)
         # Initialise the logger
-        self.log = logger.get_logger(__name__)
+        self.log = logger.setup_logger(__name__, 'logs/overwatch.log')
         # Variables
         self.turns = 0
         self.pct_explored = 0
-        self.visited_nodes = []
+        self.visited_junctions = []
         self.agent_positions = {}
-        self.all_nodes = self.environment.node_names
+        self.all_junctions = self.environment.junction_names
 
     def update(self):
         """
@@ -28,12 +28,16 @@ class OverWatch:
         """
         # Update the number of turns
         self.turns += 1
+        self.log.info(f"Turn {self.turns}")
         # Update the percentage of nodes explored
         self.pct_explored = self.get_pct_explored()
+        self.log.info(f"Percentage of nodes explored: {self.pct_explored}")
         # Update the agent positions
         self.agent_positions = self.get_agent_positions()
+        self.log.info(f"Agent positions: {self.agent_positions}")
         # Update the visited nodes
-        self.visited_nodes = self.get_visited_nodes()
+        self.visited_junctions = self.get_visited_junctions()
+        self.log.info(f"Visited nodes: {self.visited_junctions}")
 
     def get_agent_positions(self):
         """
@@ -46,17 +50,17 @@ class OverWatch:
         # Return the dictionary
         return self.agent_positions
 
-    def get_visited_nodes(self):
+    def get_visited_junctions(self):
         """
         Method to get the unique visited nodes of the agents
         :return: Dictionary of unique visited nodes
         """
         # Get the unique visited nodes of the agents
         for agent in self.agents:
-            self.visited_nodes.extend(agent.visited_nodes)
-        self.visited_nodes = list(set(self.visited_nodes))
+            self.visited_junctions.extend(agent.visited_junctions)
+        self.visited_junctions = list(set(self.visited_junctions))
         # Return the dictionary
-        return self.visited_nodes
+        return self.visited_junctions
 
     def get_pct_explored(self):
         """
@@ -64,6 +68,6 @@ class OverWatch:
         :return: Percentage of nodes explored
         """
         # Get the percentage of nodes explored
-        self.pct_explored = (len(self.visited_nodes) / len(self.all_nodes)) * 100
+        self.pct_explored = (len(self.visited_junctions) / len(self.all_junctions)) * 100
         # Return the percentage
         return self.pct_explored
