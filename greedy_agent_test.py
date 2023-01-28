39828f724e1c4fbd83090254d1d89edb151db8a8
el20sw <el20sw@leeds.ac.uk>
2023-01-28 16:25:31 +0000

    return to single agent testing

--- greedy_agent_test.py
+++ greedy_agent_test.py
@@ -21,27 +21,32 @@ sim = Simulation(env)
 # Create agent
 log.debug('Creating agent')
 agent = GreedyAgent(env, 0, '11')
-# Create agent
-log.debug('Creating agent')
-agent2 = GreedyAgent(env, 1, '22')
 
 # Add agents to simulation
 log.debug('Adding agents to simulation')
 sim.add_agent(agent)
-sim.add_agent(agent2)
 
-# Print agents
-print(sim.agents)
+# Log the agents
+log.info(f'Agents: {sim.agents}')
 
 # Run simulation
 log.debug('Running simulation')
-sim.run(max_turns=1000)
+sim.run(max_turns=100)
 
 # Get results
 results = sim.get_results()
 
 # Write results to file
 sim.write_results_to_file()
+
+# Get the possible junctions in ordered form
+junctions = env.get_junction_names()
+junctions = sorted(junctions)
+log.info(f'All Junctions: {junctions}')
+# Get the visited junctions
+visited_junctions = results['visited_junctions']
+visited_junctions = sorted(visited_junctions)
+log.info(f'Visited Junctions: {visited_junctions}')
 
 # Render the network
 env.render_network(node_labels=True, link_labels=True)
