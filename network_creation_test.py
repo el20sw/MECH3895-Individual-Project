4bef81286c8213b0476bdc31b1ce7f4e265e9b9f
el20sw <el20sw@leeds.ac.uk>
2023-01-29 11:58:23 +0000

    update network test script for visuals

--- network_creation_test.py
+++ network_creation_test.py
@@ -5,6 +5,8 @@ This script tests the creation of a network object from an EPANET input file.
 import debug.logger as logger
 # Import network
 from src.network import Network
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
 
 # Initialise logger
 logger = logger.setup_logger('network', './logs/network.log', level='DEBUG')
@@ -29,3 +31,16 @@ net3 = Network('networks/Net3.inp')
 logger.info(f"Network: {net3}")
 # Write adjacency list to file
 net3.write_adj_list_to_file('data/Net3_adj_list.json')
+
+# Create figure to contain plots of networks
+fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+# Set figure title
+fig.suptitle('Networks')
+# Plot network 1
+net1.plot_network(ax=axs[0])
+# Plot network 2
+net2.plot_network(ax=axs[1])
+# Plot network 3
+net3.plot_network(ax=axs[2])
+# Show plots
+plt.show()
