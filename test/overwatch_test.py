089277a70c122237794231a06b41867d5a199fb4
el20sw <el20sw@leeds.ac.uk>
2023-02-01 13:55:28 +0000

    basic overwatch testing script - wip

---
+++ test/overwatch_test.py
@@ -0,0 +1,30 @@
+# Append the parent directory to the path
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import unittest
+import tracemalloc
+import src.debug.logger as logger
+from src.network import Network
+from src.random_agent import RandomAgent
+from src.overwatch import Overwatch
+
+class TestOverwatch(unittest.TestCase):
+    """
+    Test Case for Overwatch
+    """
+    def setUp(self):
+        tracemalloc.start()
+        self.logger = logger.get_logger(__name__)
+        self.net1 = Network('networks/Net1.inp')
+        self.agent1 = RandomAgent(self.net1, 'A', '11')
+        self.agent2 = RandomAgent(self.net1, 'B', '10')
+        self.agent3 = RandomAgent(self.net1, 'C', '9')
+        self.overwatch = Overwatch(self.net1, [self.agent1, self.agent2, self.agent3])
+
+    def test_overwatch(self):
+        self.assertIsInstance(self.overwatch, Overwatch)
+
+if __name__ == '__main__':
+    unittest.main()
