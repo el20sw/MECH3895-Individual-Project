72d700ba8860e4389dd7ed0a719ee9bbc337c8b9
el20sw <el20sw@leeds.ac.uk>
2023-02-04 11:04:58 +0000

    belief unit test

--- test/belief_test.py
+++ test/belief_test.py
@@ -4,89 +4,150 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import unittest
-import tracemalloc
 
-import src.debug.logger as logger
 from src.network import Network
-from src.overwatch import Overwatch
-from src.random_agent import RandomAgent
+from src.belief import Belief
+from src.observation import Observation
 from src.transmittable import Transmittable
 
-class TestBelief(unittest.TestSuite):
-    def __init__(self):
-        super().__init__()
-        self.addTest(TestUpdateBelief('test_update_belief'))
 
-class TestUpdateBelief(unittest.TestCase):
+class TestBelief(unittest.TestCase):
     def setUp(self):
-        tracemalloc.start()
-        self.logger = logger.setup_logger(__name__, 'logs/test_update_belief.log', 'DEBUG')
-        self.net1 = Network('networks/Net1.inp')
-        self.agent1 = RandomAgent(self.net1, 'A', '11')
-        self.agent2 = RandomAgent(self.net1, 'B', '22')
-        self.agent3 = RandomAgent(self.net1, 'C', '32')
-        self.overwatch = Overwatch(self.net1, [self.agent1, self.agent2, self.agent3])
+        self.network = Network('networks/Net1.inp')
+        self.belief = Belief(self.network, 'A', '11')
 
-    def test_update_belief_part1(self):
-        self.logger.debug('Testing update_belief()')
-        self.logger.debug('Initialising agents')
-        self.agent1.observe(self.net1)
-        self.agent2.observe(self.net1)
-        self.agent3.observe(self.net1)
-        belief1 = self.agent1.belief
-        belief2 = self.agent2.belief
-        belief3 = self.agent3.belief
-        self.logger.debug('Initialising transmittables')
-        transmittable_b1 = Transmittable(belief1)
-        transmittable_b2 = Transmittable(belief2)
-        transmittable_b3 = Transmittable(belief3)
-        self.logger.debug('Updating beliefs')
-        self.agent1.update_belief(transmittable_b2, transmittable_b3)
-        self.agent2.update_belief(transmittable_b1, transmittable_b3)
-        self.agent3.update_belief(transmittable_b1, transmittable_b2)
-        self.logger.debug('Checking beliefs')
-        agent1_end_belief = {'10': 1, '11': 0, '12': 1, '13': 1, '21': 1, '22': -10, '23': 1, '31': 1, '32': -10, '2': 1, '9': 1}
-        agent2_end_belief = {'10': 1, '11': -10, '12': 1, '13': 1, '21': 1, '22': 0, '23': 1, '31': 1, '32': -10, '2': 1, '9': 1}
-        agent3_end_belief = {'10': 1, '11': -10, '12': 1, '13': 1, '21': 1, '22': -10, '23': 1, '31': 1, '32': 0, '2': 1, '9': 1}
-        self.assertDictEqual(self.agent1.belief.nodes, agent1_end_belief)
-        self.assertDictEqual(self.agent2.belief.nodes, agent2_end_belief)
-        self.assertDictEqual(self.agent3.belief.nodes, agent3_end_belief)
+    def test_belief(self):
+        """
+        Test that the belief is an instance of the Belief class
+        """
+        self.assertIsInstance(self.belief, Belief)
 
-    def test_update_belief_part2(self):
-        self.logger.debug('Testing update_belief()')
-        self.logger.debug('Moving agents')
-        self.agent1._position = '12'
-        self.agent2._position = '21'
-        self.agent3._position = '31'
-        self.logger.debug('Initialising agents')
-        self.agent1.observe(self.net1)
-        self.agent2.observe(self.net1)
-        self.agent3.observe(self.net1)
-        belief1 = self.agent1.belief
-        belief2 = self.agent2.belief
-        belief3 = self.agent3.belief
-        self.logger.debug('Initialising transmittables')
-        transmittable_b1 = Transmittable(belief1)
-        transmittable_b2 = Transmittable(belief2)
-        transmittable_b3 = Transmittable(belief3) 
-        self.logger.debug('Updating beliefs')
-        self.agent1.update_belief(transmittable_b2, transmittable_b3)
-        self.agent2.update_belief(transmittable_b1, transmittable_b3)
-        self.agent3.update_belief(transmittable_b1, transmittable_b2)
-        self.logger.debug('Checking beliefs')
-        agent1_end_belief = {'10': 1, '11': 0, '12': 0, '13': 1, '21': -10, '22': 0, '23': 1, '31': -10, '32': 0, '2': 1, '9': 1}
-        agent2_end_belief = {'10': 1, '11': 0, '12': -10, '13': 1, '21': 0, '22': 0, '23': 1, '31': -10, '32': 0, '2': 1, '9': 1}
-        agent3_end_belief = {'10': 1, '11': 0, '12': -10, '13': 1, '21': -10, '22': 0, '23': 1, '31': 0, '32': 0, '2': 1, '9': 1}
-        self.assertDictEqual(self.agent1.belief.nodes, agent1_end_belief)
-        self.assertDictEqual(self.agent2.belief.nodes, agent2_end_belief)
-        self.assertDictEqual(self.agent3.belief.nodes, agent3_end_belief)
+    def test_property_agent(self):
+        """
+        Test that the agent_id property is set correctly
+        """
+        self.assertEqual(self.belief.agent_id, 'A')
 
-    def tearDown(self):
-        self.logger.debug('Testing complete')
-        self.logger.debug('Testing memory usage')
-        current, peak = tracemalloc.get_traced_memory()
-        self.logger.debug(f'Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB')
-        tracemalloc.stop()
+    def test_property_position(self):
+        """
+        Test that the position property is set correctly
+        """
+        self.assertEqual(self.belief.position, '11')
+        
+    def test_property_nodes(self):
+        """
+        Test that the nodes property is set correctly
+        """
+        self.assertIsInstance(self.belief.nodes, dict)
+        self.assertDictEqual(self.belief.nodes, {'10': 1, '11': 0, '12': 1, '13': 1, '21': 1, '22': 1, '23': 1, '31': 1, '32': 1, '2': 1, '9': 1})
+
+    def test_property_unvisited_nodes(self):
+        """
+        Test that the unvisited_nodes property is set correctly
+        """
+        self.assertIsInstance(self.belief.unvisited_nodes, list)
+        self.assertListEqual(self.belief.unvisited_nodes, ['10', '12', '13', '21', '22', '23', '31', '32', '2', '9'])
+
+    def test_property_visited_nodes(self):
+        """
+        Test that the visited_nodes property is set correctly
+        """
+        self.assertIsInstance(self.belief.visited_nodes, list)
+        self.assertListEqual(self.belief.visited_nodes, ['11'])
+
+    def test_property_occupied_nodes(self):
+        """
+        Test that the occupied_nodes property is set correctly
+        """
+        self.assertIsInstance(self.belief.occupied_nodes, list)
+        self.assertListEqual(self.belief.occupied_nodes, [])
+
+    def test_property_links(self):
+        """
+        Test that the links property is set correctly
+        """
+        self.assertIsInstance(self.belief.links, list)
+        self.assertListEqual(self.belief.links, [])
+
+    def test_method_update(self):
+        """
+        Test that the update method updates the belief correctly
+        """
+        self.observation = Observation(self.network, '12')
+        self.belief.update(self.observation)
+        self.assertEqual(self.belief.position, '12')
+        self.assertDictEqual(self.belief.nodes, {'10': 1, '11': 0, '12': 0, '13': 1, '21': 1, '22': 1, '23': 1, '31': 1, '32': 1, '2': 1, '9': 1})
+        self.assertListEqual(self.belief.unvisited_nodes, ['10', '13', '21', '22', '23', '31', '32', '2', '9'])
+        self.assertListEqual(self.belief.visited_nodes, ['11', '12'])
+        self.assertListEqual(self.belief.occupied_nodes, [])
+        self.assertListEqual(self.belief.links, [('12', '11', {'link_name' : self.network.adj_list['12']['11']['link_name'], 'link_length' : self.network.adj_list['12']['11']['link_length']})])
+
+        """
+        Test that the update_from_observation method updates the belief correctly
+        """
+        self.observation = Observation(self.network, '22')
+        self.belief.update(self.observation)
+        self.assertEqual(self.belief.position, '22')
+        self.assertDictEqual(self.belief.nodes, {'10': 1, '11': 0, '12': 0, '13': 1, '21': 1, '22': 0, '23': 1, '31': 1, '32': 1, '2': 1, '9': 1})
+        self.assertListEqual(self.belief.unvisited_nodes, ['10', '13', '21', '23', '31', '32', '2', '9'])
+        self.assertListEqual(self.belief.visited_nodes, ['11', '12', '22'])
+        self.assertListEqual(self.belief.occupied_nodes, [])
+        self.assertListEqual(self.belief.links, [
+            ('12', '11', {'link_name' : self.network.adj_list['12']['11']['link_name'], 'link_length' : self.network.adj_list['12']['11']['link_length']}), 
+            ('22', '12', {'link_name' : self.network.adj_list['22']['12']['link_name'], 'link_length' : self.network.adj_list['22']['12']['link_length']})])
+
+        """
+        Test that the update_from_communication method updates the belief correctly
+        """
+
+        # New 'agent' at position '32'
+        beliefA = Belief(self.network, 'B', '32')
+        self.assertEqual(beliefA.position, '32')
+
+        # New 'agent' moves to position '31
+        observationA = Observation(self.network, '31')
+        beliefA.update(observationA)
+        self.assertEqual(beliefA.position, '31')
+
+        transmittable = Transmittable(beliefA)
+
+        self.belief.update(transmittable)
+        self.assertEqual(self.belief.position, '22')
+        self.assertDictEqual(self.belief.nodes, {'10': 1, '11': 0, '12': 0, '13': 1, '21': 1, '22': 0, '23': 1, '31': -10, '32': 0, '2': 1, '9': 1})
+        self.assertListEqual(self.belief.unvisited_nodes, ['10', '13', '21', '23', '2', '9'])
+        self.assertListEqual(self.belief.visited_nodes, ['11', '12', '22', '32'])
+        self.assertListEqual(self.belief.occupied_nodes, ['31'])
+        self.assertListEqual(self.belief.links, [
+            ('12', '11', {'link_name' : self.network.adj_list['12']['11']['link_name'], 'link_length' : self.network.adj_list['12']['11']['link_length']}), 
+            ('22', '12', {'link_name' : self.network.adj_list['22']['12']['link_name'], 'link_length' : self.network.adj_list['22']['12']['link_length']}), 
+            ('31', '32', {'link_name' : self.network.adj_list['31']['32']['link_name'], 'link_length' : self.network.adj_list['31']['32']['link_length']}),
+            ])
+
+        # 'Agent' moves to position '23
+        self.observation = Observation(self.network, '23')
+        self.belief.update(self.observation)        
+
+        # New 'agent' at position '21'
+        observationB = Observation(self.network, '21')
+        beliefA.update(observationB)
+        self.assertEqual(beliefA.position, '21')
+
+        transmittable = Transmittable(beliefA)
+
+        self.belief.update(transmittable)
+        self.assertEqual(self.belief.position, '23')
+        self.assertDictEqual(self.belief.nodes, {'10': 1, '11': 0, '12': 0, '13': 1, '21': -10, '22': 0, '23': 0, '31': 0, '32': 0, '2': 1, '9': 1})
+        self.assertListEqual(self.belief.unvisited_nodes, ['10', '13', '2', '9'])
+        self.assertListEqual(self.belief.visited_nodes, ['11', '12', '22', '23', '31', '32'])
+        self.assertListEqual(self.belief.occupied_nodes, ['21'])
+        self.assertListEqual(self.belief.links, [
+            ('12', '11', {'link_name' : self.network.adj_list['12']['11']['link_name'], 'link_length' : self.network.adj_list['12']['11']['link_length']}),
+            ('22', '12', {'link_name' : self.network.adj_list['22']['12']['link_name'], 'link_length' : self.network.adj_list['22']['12']['link_length']}),
+            ('31', '32', {'link_name' : self.network.adj_list['31']['32']['link_name'], 'link_length' : self.network.adj_list['31']['32']['link_length']}),
+            ('23', '22', {'link_name' : self.network.adj_list['23']['22']['link_name'], 'link_length' : self.network.adj_list['23']['22']['link_length']}),
+            ('21', '31', {'link_name' : self.network.adj_list['21']['31']['link_name'], 'link_length' : self.network.adj_list['21']['31']['link_length']}),            
+            ])
+        
 
 if __name__ == '__main__':
     unittest.main()
