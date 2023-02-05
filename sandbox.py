e7b07627c8e1902a63f7ce03c3e3ed799c819e24
el20sw <el20sw@leeds.ac.uk>
2023-02-05 12:11:49 +0000

    add 3rd agent to test script

--- sandbox.py
+++ sandbox.py
@@ -13,15 +13,16 @@ def main():
     # Initialise the simulation
     log.info('Initialising the simulation')
     # Create the environment layer
-    env = Network('networks/Net2.inp')
+    env = Network('networks/Net1.inp')
     log.debug(f'Environment: {env}')
     # Create the agent layer
     agentA = GreedyAgent(env, 'A', '11', communication_range=-1, random_seed=0)
     agentB = GreedyAgent(env, 'B', '31', communication_range=-1, random_seed=0)
+    agentC = GreedyAgent(env, 'C', '23', communication_range=-1, random_seed=0)
     log.debug(f'{agentA} @ {agentA.position}')
     log.debug(f'{agentB} @ {agentB.position}')
     # Create the simulation layer
-    agent_list = [agentA, agentB]
+    agent_list = [agentA, agentB, agentC]
     simulation = Simulation(env)
     log.debug(f'Simulation: {simulation}')
     # Add agents to the simulation
@@ -30,8 +31,11 @@ def main():
     log.debug(f'Overwatch Agent Numbers: {simulation.overwatch.num_agents}')
     log.debug(f'Overwatch Agent Positions: {simulation.overwatch.agent_positions}')
     log.debug(f'Overwatch Comms Buffer: {simulation.overwatch.communication_buffer}')
+
     simulation.add_agent(agentA)
-    # simulation.add_agent(agentB)
+    simulation.add_agent(agentB)
+    simulation.add_agent(agentC)
+
     log.debug(f'Simulation Agents: {simulation.agents}')
     log.debug(f'Overwatch Agents: {simulation.overwatch.agents}')
     log.debug(f'Overwatch Agent Numbers: {simulation.overwatch.num_agents}')
