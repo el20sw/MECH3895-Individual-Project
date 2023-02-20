0471ab11d914c97743eaf08a75a588aa324e1666
el20sw <el20sw@leeds.ac.uk>
2023-02-20 15:43:50 +0000

    begin agent start position placement setting

--- src/agent_generator.py
+++ src/agent_generator.py
@@ -14,7 +14,7 @@ from src.network import Network
 
 log = logger.get_logger(__name__)
 
-def generate_agents(env: Network, num_agents: int, num_start_pos:int=1, random_seed:int=0) -> List[Agent]:
+def generate_agents(env: Network, num_agents: int, random_seed:int=0  ) -> List[Agent]:
     """
     Method to generate a list of agents for the simulation
     
@@ -23,20 +23,22 @@ def generate_agents(env: Network, num_agents: int, num_start_pos:int=1, random_s
     
     env: environment for the agents to be generated in
     num_agents: number of agents to generate
-    num_start_pos: number of start positions to generate
     random_seed: random seed for the generation of the agents
     
     Returns
     -------
     return: list of agents
-    """   
+    """
+    
+    # Ask the user for the start positions
+    # start_positions = ask_start_positions(env)
     
     # Generate the agents
-    agents = [Agent(env, agent_id, start_pos) for agent_id, start_pos in generate_start_positions(env, num_agents, num_start_pos, random_seed)]
+    agents = [Agent(env, agent_id, start_pos) for agent_id, start_pos in generate_start_positions(env, num_agents)]
     # Return the agents
     return agents
 
-def generate_start_positions(env: Network, num_agents: int, num_start_pos:int=1, random_seed:int=0) -> List[tuple]:
+def generate_start_positions(env: Network, num_agents: int, start_postitions=None, random_seed:int=0) -> List[tuple]:
     """
     Method to generate a list of start positions for the agents
     
@@ -55,23 +57,42 @@ def generate_start_positions(env: Network, num_agents: int, num_start_pos:int=1,
     # Set the random seed
     random.seed(random_seed)
     
-    # # Create TKinter containing the map of the network and a selection of start positions
-    # root = tk.Tk()
-    # root.title("Select Start Positions")
-    # root.geometry("800x600")
-    # root.resizable(False, False)
-    # # Create the canvas
-    # canvas = tk.Canvas(root, width=800, height=600)
-    # canvas.pack()
-    # # Create the map image
-    # wntr.graphics.plot_network(env.water_network_model, node_size=5, node_labels=True, filename="temp/map.png")
-    # # Create the map
-    # map = tk.PhotoImage(file="temp/map.png")
-    # canvas.create_image(0, 0, anchor=tk.NW, image=map)
-    # # Create the start positions
-    # start_positions = []
+    # If the start positions are not given
+    if start_postitions is None:
+        # Generate the start positions
+        start_positions = [(agent_id, 'Lake') for agent_id in range(num_agents)]
+        return start_positions
     
-    # Generate the start positions
-    start_positions = [(agent_id, 'Lake') for agent_id in range(num_agents)]
+    start_positions = []
     # Return the start positions
     return start_positions
+    
+def ask_start_positions(env: Network):
+    """
+    Method to ask the user for the start positions of the agents
+    """
+    
+    # Get the list of nodes
+    nodes = list(env.node_names)
+    # Create TKinter window for the selection of the start positions
+    root = tk.Tk()
+    # Create the listbox
+    listbox = tk.Listbox(root, selectmode=tk.MULTIPLE)
+    # Add the nodes to the listbox
+    for node in nodes:
+        listbox.insert(tk.END, node)
+    # Pack the listbox
+    listbox.pack()
+    # Create the button
+    button = tk.Button(root, text='OK', command=root.destroy)
+    # Pack the button
+    button.pack()
+    # Run the mainloop
+    root.mainloop()
+    
+    # Get the selected nodes
+    
+    
+    
+    return None
+        
