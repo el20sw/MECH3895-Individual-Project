7a537d221ebfbef394fb8b73da83e5895a05d480
el20sw <el20sw@leeds.ac.uk>
2023-03-20 11:49:45 +0000

    possible to create agents with predefined start positions

--- src/agent_generator.py
+++ src/agent_generator.py
@@ -14,7 +14,7 @@ from src.network import Network
 
 log = logger.get_logger(__name__)
 
-def generate_agents(env: Network, num_agents: int, threshold=None, random_seed:int=0) -> List[Agent]:
+def generate_agents(env: Network, num_agents: int, threshold=None, random_seed:int=0, start_positions=None) -> List[Agent]:
     """
     Method to generate a list of agents for the simulation
     
@@ -29,6 +29,8 @@ def generate_agents(env: Network, num_agents: int, threshold=None, random_seed:i
         threshold for the agents to use when allocating tasks in an informed way
     random_seed: int, optional (default=0)
         random seed for the generation of the agents
+    start_positions: list of start positions, optional (default=None)
+        list of start positions for the agents
     
     Returns
     -------
@@ -38,14 +40,20 @@ def generate_agents(env: Network, num_agents: int, threshold=None, random_seed:i
     # Ask the user for the start positions
     # start_positions = ask_start_positions(env)
     
-    print("Creating agents...")
+    if start_positions is None:
+        print("Creating agents...")
     
     if isinstance(threshold, str):
         # process the threshold string
         threshold = threshold.lower().strip()
     
-    # Generate the agents
-    agents = [Agent(env, agent_id, start_pos, threshold) for agent_id, start_pos in generate_start_positions(env, num_agents)]
+    # If start_positions is None, generate the start positions
+    if start_positions is None:
+        # Generate the agents
+        agents = [Agent(env, agent_id, start_pos, threshold) for agent_id, start_pos in generate_start_positions(env, num_agents)]
+        return agents
+        
+    agents = [Agent(env, agent_id, start_pos, threshold) for agent_id, start_pos in assign_start_positions(env, num_agents, start_positions)]
     # Return the agents
     return agents
 
@@ -114,4 +122,37 @@ def ask_start_positions(env: Network):
             raise ValueError(f"Start position {start_pos} is not a valid node in the network")
     
     return start_positions
+
+def assign_start_positions(env: Network, num_agents: int, start_positions: List[str]):
+    """
+    Method to assign agents to given start positions
+    
+    Parameters
+    ----------
+    
+    env: Network
+        environment for the agents to be generated in
+    num_agents: int
+        number of agents to generate
+    start_pos: list of nodes
+        list of nodes to assign agents to
+    
+    Returns
+    -------
+    list of agent start positions
+    """
+    
+    # Check that the start positions are valid
+    for start_pos in start_positions:
+        if start_pos not in env.node_names:
+            log.critical(f"Start position {start_pos} is not a valid node in the network")
+            raise ValueError(f"Start position {start_pos} is not a valid node in the network")
+        
+    # Assign the start positions to the agents - round robin style so agents are evenly distributed across the start positions
+    agent_pos = [(agent_id, start_positions[agent_id % len(start_positions)]) for agent_id in range(num_agents)]
+    
+    # Return the start positions
+    return agent_pos
+    
+        
         
