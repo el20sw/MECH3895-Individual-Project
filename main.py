af30c509b633ed86d4292b595c3e2d94edbcef67
el20sw <el20sw@leeds.ac.uk>
2023-03-20 11:50:17 +0000

    remove unused import

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import logging
 
 from src.simulation import Simulation
 from src.network import Network
-from src.agent import Agent
 from src.render import Render
 
 logging.disable(logging.CRITICAL)
